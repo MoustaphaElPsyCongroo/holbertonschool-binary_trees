d256867682793ce9715d08f007555b6579c82351
MousElPsyCongroo <mtapha.tall@gmail.com>
2022-08-29 14:58:35 +0200

    0 - Correct function  documentation and null tree case

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -3,11 +3,16 @@
 /**
  * binary_tree_depth - Calculates the depth of a node
  * @tree: A pointer to the node
+ *
+ * Return: The depth of the node, with 0 if no tree
  */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
 	size_t i = 0;
 	binary_tree_t *current = (binary_tree_t *)tree;
+
+	if (!current->parent)
+		return (0);
 
 	while (current->parent)
 	{
