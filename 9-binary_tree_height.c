32eb8d5d6744e88a29277778eb4d8b48b9431369
MousElPsyCongroo <mtapha.tall@gmail.com>
2022-08-30 04:07:50 -0700

    9 - Correct null tree case

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,17 +8,21 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	int left_height;
-	int right_height;
+	size_t left_ht;
+	size_t right_ht;
 
 	if (!tree)
-		return (-1);
+	{
+		return (0);
+	}
+	if (!tree->left && !tree->right)
+		return (0);
 
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);
+	left_ht = binary_tree_height(tree->left);
+	right_ht = binary_tree_height(tree->right);
 
-	if (left_height > right_height)
-		return (left_height + 1);
+	if (left_ht > right_ht)
+		return (left_ht + 1);
 	else
-		return (right_height + 1);
+		return (right_ht + 1);
 }
